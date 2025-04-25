0054cf4d0f17a70506ce3169b1eeb51a09b518d4
Stasmorkva <stasrusak80@gmail.com>
2025-04-25 18:09:48 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,5 @@
+https://github.com/AnneliNova/LogiTalkProject/blob/main/.gitignore
+https://github.com/AnneliNova/LogiTalkProject/blob/main/LICENSE
+https://github.com/AnneliNova/LogiTalkProject/blob/main/README.md
+https://github.com/AnneliNova/LogiTalkProject/blob/main/proUI.py
+https://github.com/AnneliNova/LogiTalkProject/blob/main/server.py
